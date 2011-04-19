e7c5b68b5fdf1f7a1a024b5bc20a3894531f9162
Ramon Roche <mrpollo@gmail.com>
2011-04-19 17:04:20 -0500

    adding initial php classes

---
+++ google.php
@@ -0,0 +1,28 @@
+<?php
+class ggeo{
+	public $url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false";
+	public $location;
+	public $format;
+	function __construct($format){
+		$this->format = $format;
+	}
+	function look($address){
+		$this->location = $address;
+		return $this->load_json_from_url($this->buildquery($address));
+	}
+	function buildquery($address){
+		$query = $this->url."&address=".urlencode($address);
+		return $query;
+	}
+	function load_file_from_url($url) {
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$str = curl_exec($curl);
+		curl_close($curl);
+		return $str;
+	}
+	function load_json_from_url($url) {
+		return json_decode($this->load_file_from_url($url));
+	}
+}
---
+++ yahoo.php
@@ -0,0 +1,52 @@
+<?php
+class ygeo{
+	// get your own APP ID
+	public $appid = 'i4lyxm34';
+	public $url = "http://where.yahooapis.com/geocode";
+	public $location;
+	public $format;
+	public $query = array();
+	function __construct($format){
+		$this->format = $format;
+	}
+	function look($address){
+		$this->location = $address;
+		return $this->load_json_from_url($this->buildquery($address));
+	}
+	function lookPostal($postal){
+		$address = new stdClass();
+		$address->postal = $postal;
+		return $this->load_json_from_url($this->buildquery($address, 2));
+	}
+	function lookSpecific($addr, $city, $state, $postal){
+		$address = new stdClass();
+		$address->addr = $addr;
+		$address->city = $city;
+		$address->state = $state;
+		$address->postal = $postal;
+		return $this->load_json_from_url($this->buildquery($address, 0));
+	}
+	function buildquery($address, $type=1){
+		$query = $this->url."?appid=".$this->appid."&flags=".$this->format;
+		if($type == 0){
+			$query .= "&addr=".urlencode($address->addr)."csz=".urlencode($address->city).",".urlencode($address->state).",".urlencode($address->postal);
+		}else if($type == 1){
+			$query .= "&location=".urlencode($address);
+		}else if($type == 2){
+			$query .= "&postal=".urlencode($address->postal)."&country=USA";
+		}
+		$this->query[] = $query;
+		return $query;
+	}
+	function load_file_from_url($url) {
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$str = curl_exec($curl);
+		curl_close($curl);
+		return $str;
+	}
+	function load_json_from_url($url) {
+		return json_decode($this->load_file_from_url($url));
+	}
+}
